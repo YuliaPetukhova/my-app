3e63099ddee6b28de7137f760f16f43b49db703a
YuliaPetukhova <yuljaschik99@gmail.com>
2023-10-25 10:54:20 +0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FilterProductsPipe } from './pipes/filter-products.pipe';
 import { ModalComponent } from './components/modal/modal.component';
 import { CreateProductComponent } from './components/create-product/create-product.component';
-import { FocusDirective } from './directives/focus.directive'
+import { FocusDirective } from './directives/focus.directive';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { NavigationComponent } from './components/navigation/navigation.component'
 
 @NgModule({
   declarations: [
@@ -21,6 +24,9 @@ import { FocusDirective } from './directives/focus.directive'
     ModalComponent,
     CreateProductComponent,
     FocusDirective,
+    ProductPageComponent,
+    AboutPageComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
